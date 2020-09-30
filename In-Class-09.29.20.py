f5a6730aca5fd7f90e500ef07a169d8d72157c17
David <dave4dl@gmail.com>
2020-09-29 19:52:57 -0700

    Added in the get and set functions

--- In-Class-09.29.20.py
+++ In-Class-09.29.20.py
@@ -17,6 +17,33 @@ class Person:
         self.firstName = ""
         self.lastName = ""
         self.dateOfBirth = datetime(1000, 1, 1)
+        
+    # ------------------- Begin Getter/Setter Function Section --------------------
+    # Function to return the value of the "firstName" attribute
+    def getFirstName(self):
+        return self.firstName
+    
+    # Function to set the value of the "firstName" attribute
+    def setFirstName(self,firstName):
+        self.firstName=firstName
+
+    # Function to return the value of the "lastName" attribute
+    def getLastName(self):
+        return self.lastName
+    
+    # Function to set the value of the "lastName" attribute
+    def setLastName(self,lastName):
+        self.lastName=lastName
+
+    # Function to return the value of the "dateOfBirth" attribute
+    def getDateOfBirth(self):
+        return self.dateOfBirth
+    
+    # Function to set the value of the "dateOfBirth" attribute
+    def setDateOfBirth(self,dateOfBirth):
+        self.dateOfBirth=dateOfBirth
+    # -------------------- End Getter/Setter Function Section ---------------------
+
 
     #__str__
 
@@ -28,7 +55,34 @@ class Actor(Person):
         self.bio = ""
         self.filmography = []
 
-    # include a method for resetting an attribute from a previously inputted value
+    # ------------------- Begin Getter/Setter Function Section --------------------
+    # Function to return the value of the "nationality" attribute
+    def getNationality(self):
+        return self.nationality
+    
+    # Function to set the value of the "nationality" attribute
+    def setNationality(self,nationality):
+        self.nationality=nationality
+
+    # Function to return the value of the "bio" attribute
+    def getBio(self):
+        return self.bio
+    
+    # Function to set the value of the "bio" attribute
+    def setBio(self,bio):
+        self.bio=bio
+
+    # Function to return the value of the "filmography" attribute
+    def getFilmography(self):
+        return self.filmography
+    
+    # Function to set the value of the "filmography" attribute
+    def setFilmography(self,filmography):
+        self.filmography=filmography
+    # -------------------- End Getter/Setter Function Section ---------------------
+
+
+
 
     #__str__
 
@@ -43,23 +97,96 @@ class Movie:
         self.watchCount = 0
         self.borrowedFlag = False
         self.borrower = Person()
+        
+    # ------------------- Begin Getter/Setter Function Section --------------------
+    # Function to return the value of the "title" attribute
+    def getTitle(self):
+        return self.title
+    
+    # Function to set the value of the "title" attribute
+    def setTitle(self,title):
+        self.title=title
 
-    # getters and setters
-        # set title, etc
-        # also give review
+    # Function to return the value of the "genre" attribute
+    def getGenre(self):
+        return self.genre
+    
+    # Function to set the value of the "genre" attribute
+    def setGenre(self,genre):
+        self.genre=genre
+
+    # Function to return the value of the "year" attribute
+    def getYear(self):
+        return self.year
+    
+    # Function to set the value of the "year" attribute
+    def setYear(self,year):
+        self.year=year
+
+    # Function to return the value of the "review" attribute
+    def getReview(self):
+        return self.review
+    
+    # Function to set the value of the "review" attribute
+    def setReview(self,review):
+        self.review=review
+
+    # Function to return the value of the "actors" attribute
+    def getActors(self):
+        return self.actors
+    
+    # Function to set the value of the "actors" attribute
+    def setActors(self,actors):
+        self.actors=actors
+
+    # Function to return the value of the "watchCount" attribute
+    def getWatchCount(self):
+        return self.watchCount
+    
+    # Function to set the value of the "watchCount" attribute
+    def setWatchCount(self,watchCount):
+        self.watchCount=watchCount
+
+    # Function to return the value of the "borrowedFlag" attribute
+    def getBorrowedFlag(self):
+        return self.borrowedFlag
+    
+    # Function to set the value of the "borrowedFlag" attribute
+    def setBorrowedFlag(self,borrowedFlag):
+        self.borrowedFlag=borrowedFlag
+
+    # Function to return the value of the "borrower" attribute
+    def getBorrower(self):
+        return self.borrower
+    
+    # Function to set the value of the "borrower" attribute
+    def setBorrower(self,borrower):
+        self.borrower=borrower
+    # -------------------- End Getter/Setter Function Section ---------------------
+
+
+    # --------------------- Begin Function Definition Section ---------------------
 
     # increase movie count
-
+    def incrementWatchCount(self,numberToIncrementBy):
+        self.watchCount+=numberToIncrementBy
 
     def checkIn(self):
         self.borrowedFlag = False
         self.borrower = None
 
-    def checkOut(self,personInput):
+    def checkOut(self,personCheckingOut):
         self.borrowedFlag = True
         self.borrower = personInput
 
     # __str__
-
+    # ---------------------- End Function Definition Section ----------------------
     #
 # End Class Definitions ---------------------------------------------------------------#
+
+
+def TestMovieClasses():
+    
+    testMovie = Movie()
+    testMovie.
+    
