cb757c004ab113b44551d682ed61ebe526aad8bd
bill-roth <70240470+bill-roth@users.noreply.github.com>
2020-10-01 15:36:47 -0700

    Finishing touches
    Finished the .py script and put everything into a juypuuytrer file. Both files added with commit

--- In-Class-09.29.20.py
+++ In-Class-09.29.20.py
@@ -1,5 +1,4 @@
-# - something i don't know what it means ------------------------#
-# -*- coding: utf-8 -*-
+# Start of script -----------------------------------------------#
 """
 Created on Tue Sep 29 15:20:00 2020
 
@@ -9,6 +8,7 @@ Created on Tue Sep 29 15:20:00 2020
 
 import datetime
 
+
 # class definitions----------------------------------------------#
 
 class Person:
@@ -16,41 +16,42 @@ class Person:
     def __init__(self):
         self.firstName = ""
         self.lastName = ""
-        self.dateOfBirth = datetime(1000, 1, 1)
-        
+        self.dateOfBirth = datetime.datetime(1900, 1, 1)
+
     # ------------------- Begin Getter/Setter Function Section --------------------
     # Function to return the value of the "firstName" attribute
     def getFirstName(self):
         return self.firstName
-    
+
     # Function to set the value of the "firstName" attribute
-    def setFirstName(self,firstName):
-        self.firstName=firstName
+    def setFirstName(self, firstName):
+        self.firstName = firstName
 
     # Function to return the value of the "lastName" attribute
     def getLastName(self):
         return self.lastName
-    
+
     # Function to set the value of the "lastName" attribute
-    def setLastName(self,lastName):
-        self.lastName=lastName
+    def setLastName(self, lastName):
+        self.lastName = lastName
 
     # Function to return the value of the "dateOfBirth" attribute
     def getDateOfBirth(self):
         return self.dateOfBirth
-    
+
     # Function to set the value of the "dateOfBirth" attribute
-    def setDateOfBirth(self,dateOfBirth):
-        self.dateOfBirth=dateOfBirth
+    def setDateOfBirth(self, year, month, day):
+        self.dateOfBirth = datetime.datetime(year, month, day)
+
     # -------------------- End Getter/Setter Function Section ---------------------
 
-
-    #__str__
+    # __str__
 
 
 class Actor(Person):
 
     def __init__(self):
+        super().__init__()
         self.nationality = ""
         self.bio = ""
         self.filmography = []
@@ -59,32 +60,34 @@ class Actor(Person):
     # Function to return the value of the "nationality" attribute
     def getNationality(self):
         return self.nationality
-    
+
     # Function to set the value of the "nationality" attribute
-    def setNationality(self,nationality):
-        self.nationality=nationality
+    def setNationality(self, nationality):
+        self.nationality = nationality
 
     # Function to return the value of the "bio" attribute
     def getBio(self):
         return self.bio
-    
+
     # Function to set the value of the "bio" attribute
-    def setBio(self,bio):
-        self.bio=bio
+    def setBio(self, bio):
+        self.bio = bio
 
     # Function to return the value of the "filmography" attribute
     def getFilmography(self):
         return self.filmography
-    
+
     # Function to set the value of the "filmography" attribute
-    def setFilmography(self,filmography):
-        self.filmography=filmography
+    def setFilmography(self, filmography):
+        self.filmography = filmography
     # -------------------- End Getter/Setter Function Section ---------------------
 
+    def __str__(self):
+        stringToReturn = "{0} {1}".format(self.firstName, self.lastName)
+        return stringToReturn
 
 
 
-    #__str__
 
 class Movie:
 
@@ -97,96 +100,131 @@ class Movie:
         self.watchCount = 0
         self.borrowedFlag = False
         self.borrower = Person()
-        
+
     # ------------------- Begin Getter/Setter Function Section --------------------
     # Function to return the value of the "title" attribute
     def getTitle(self):
         return self.title
-    
+
     # Function to set the value of the "title" attribute
-    def setTitle(self,title):
-        self.title=title
+    def setTitle(self, title):
+        self.title = title
 
     # Function to return the value of the "genre" attribute
     def getGenre(self):
         return self.genre
-    
+
     # Function to set the value of the "genre" attribute
-    def setGenre(self,genre):
-        self.genre=genre
+    def setGenre(self, genre):
+        self.genre = genre
 
     # Function to return the value of the "year" attribute
     def getYear(self):
         return self.year
-    
+
     # Function to set the value of the "year" attribute
-    def setYear(self,year):
-        self.year=year
+    def setYear(self, year):
+        self.year = year
 
     # Function to return the value of the "review" attribute
     def getReview(self):
         return self.review
-    
+
     # Function to set the value of the "review" attribute
-    def setReview(self,review):
-        self.review=review
+    def setReview(self, review):
+        self.review = review
 
     # Function to return the value of the "actors" attribute
     def getActors(self):
         return self.actors
-    
+
     # Function to set the value of the "actors" attribute
-    def setActors(self,actors):
-        self.actors=actors
+    def setActors(self, actors):
+        self.actors = actors
 
     # Function to return the value of the "watchCount" attribute
     def getWatchCount(self):
         return self.watchCount
-    
+
     # Function to set the value of the "watchCount" attribute
-    def setWatchCount(self,watchCount):
-        self.watchCount=watchCount
+    def setWatchCount(self, watchCount):
+        self.watchCount = watchCount
 
     # Function to return the value of the "borrowedFlag" attribute
     def getBorrowedFlag(self):
         return self.borrowedFlag
-    
+
     # Function to set the value of the "borrowedFlag" attribute
-    def setBorrowedFlag(self,borrowedFlag):
-        self.borrowedFlag=borrowedFlag
+    def setBorrowedFlag(self, borrowedFlag):
+        self.borrowedFlag = borrowedFlag
 
     # Function to return the value of the "borrower" attribute
     def getBorrower(self):
         return self.borrower
-    
-    # Function to set the value of the "borrower" attribute
-    def setBorrower(self,borrower):
-        self.borrower=borrower
-    # -------------------- End Getter/Setter Function Section ---------------------
 
+    # Function to set the value of the "borrower" attribute
+    def setBorrower(self, borrower):
+        self.borrower = borrower
+
+    # -------------------- End Getter/Setter Function Section ---------------------
 
     # --------------------- Begin Function Definition Section ---------------------
 
     # increase movie count
-    def incrementWatchCount(self,numberToIncrementBy):
-        self.watchCount+=numberToIncrementBy
+    def incrementWatchCount(self, numberToIncrementBy):
+        self.watchCount += numberToIncrementBy
 
     def checkIn(self):
         self.borrowedFlag = False
         self.borrower = None
 
-    def checkOut(self,personCheckingOut):
+    def checkOut(self, personCheckingOut):
         self.borrowedFlag = True
         self.borrower = personInput
 
-    # __str__
+    def __str__(self):
+        genreToReturn = ""
+        for genre in self.genre:
+            if len(genreToReturn) > 0:
+                genreToReturn += ", "
+            genreToReturn += genre
+
+        actorsToReturn = ""
+        for actor in self.actors:
+            if len(actorsToReturn) > 0:
+                actorsToReturn += "\n"
+            actorsToReturn += "\t" + str(actor)
+
+        stringToReturn = "Movie: {0} \t Year: {1} \n Genre: {2} \n List of Actors: \n {3}".format(self.title, self.year,
+                                                                                                  genreToReturn,
+                                                                                                  actorsToReturn)
+        return stringToReturn
+
     # ---------------------- End Function Definition Section ----------------------
     #
+
+
 # End Class Definitions ---------------------------------------------------------------#
 
 
 def TestMovieClasses():
-    
+    testActor1 = Actor()
+    testActor1.setFirstName("Adam")
+    testActor1.setLastName("Driver")
+    testActor1.setDateOfBirth(1971,3,10)
+
+    testActor2 = Actor()
+    testActor2.setFirstName("Jon")
+    testActor2.setLastName("Hamm")
+    testActor2.setDateOfBirth(1971,3,10)
+
     testMovie = Movie()
-    testMovie.
-    
+    testMovie.setTitle("The Report")
+    testMovie.setYear(2019)
+    testMovie.setGenre(["Thriller", "Crime", "Drama"])
+    testMovie.getActors().append(testActor1)
+    testMovie.getActors().append(testActor2)
+
+    print(testMovie)
+
+TestMovieClasses()
